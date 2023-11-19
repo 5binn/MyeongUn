0221e7869a6dd282bbedd2c906d99ee2f3b40240
5binn <dhxnejqls13@naver.com>
2023-11-19 17:52:13 +0900

    5th

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -8,9 +10,10 @@ public class App {
     App(Scanner sc) {
         this.sc = sc;
     }
+    List<내용> 내용List = new ArrayList<>();
     void run() {
         System.out.println("== 명언 앱 ==");
-        long 번호 = 1;
+        int 번호 = 1;
         while (true){
             System.out.print("명령) ");
             String 명령 = this.sc.nextLine().trim();
@@ -20,13 +23,21 @@ public class App {
                 System.out.print("작가 : ");
                 String 작가 = this.sc.nextLine().trim();
                 System.out.printf("%d번 명령이 등록되었습니다.\n",번호);
+                내용 a내용 = new 내용(번호,작가,명언);
+                내용List.add(a내용);
                 번호++;
             } else if (명령.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언"+"\n---------------");
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-".repeat(30));
+                for (int i=0;i<내용List.size();i++){
+                    내용 a내용 = 내용List.get(i);
+                    System.out.printf("%d / %s / %s\n",a내용.get번호(),a내용.get작가(),a내용.get명언());
+                }
             }else if (명령.equals("종료")) {
                 break;
             }
         }
     }
 }
+
 
