562ecbfbd7577ef91c49d74966e0bc04c6a6bbb2
5binn <dhxnejqls13@naver.com>
2023-11-19 17:10:50 +0900

    2nd

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class App {
     private Scanner sc;
-    App (Scanner sc) {
+
+    App(Scanner sc) {
         this.sc = sc;
     }
+
     void run() {
         System.out.println("== 명언 앱 ==");
-        while(true){
+        while (true) {
             System.out.print("명령) ");
-            String 명령 = this.sc.nextLine();
+            String 명령 = this.sc.nextLine().trim();
             if (명령.equals("등록")) {
                 System.out.print("명언 : ");
-                sc.nextLine();
+                String 명언 = this.sc.nextLine().trim();
                 System.out.print("작가 : ");
-                sc.nextLine();
-
+                String 작가 = this.sc.nextLine().trim();
+                System.out.println("1번 명령이 등록되었습니다.");
                 continue;
-            }
-            if (명령.equals("종료")){
+            } else if (명령.equals("종료")) {
                 break;
             }
         }
