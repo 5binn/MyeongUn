ab25d13cec45091ed19a89579dd9d9dc14826b93
5binn <dhxnejqls13@naver.com>
2023-11-19 16:52:10 +0900

    2nd

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,2 +1,29 @@
-package org.example;public class App {
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private Scanner sc;
+    App (Scanner sc) {
+        this.sc = sc;
+    }
+    void run() {
+        System.out.println("== 명언 앱 ==");
+        while(true){
+            System.out.print("명령) ");
+            String 명령 = this.sc.nextLine();
+            if (명령.equals("등록")) {
+                System.out.print("명언 : ");
+                sc.nextLine();
+                System.out.print("작가 : ");
+                sc.nextLine();
+
+                continue;
+            }
+            if (명령.equals("종료")){
+                break;
+            }
+        }
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        String 명령 = sc.nextLine();
-        if(명령.equals("종료")){
-            System.out.print("종료");
+        new App(sc).run();
+        sc.close();
+
         }
     }
-}
+
